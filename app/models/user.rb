e6382d5f5a8267d240ef5b9b7d98b64bb0bf098b
fixel92 <goodzoon92@gmail.com>
2019-12-13 18:24:56 +0300

    added references User to Events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+  EMAIL_VALIDATION = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
+  has_many :events
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, uniqueness: true
+  validates :email, format: EMAIL_VALIDATION
 end
