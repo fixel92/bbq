867833bcfda2a7ee811c7506d2beaef3881fd0aa
fixel92 <goodzoon92@gmail.com>
2019-12-17 21:50:04 +0300

    added views user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,17 @@ class UsersController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
+    if @user.update(user_params)
+      respond_to do |format|
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
+    else
+      render :edit
     end
   end
 
   private
+
   def set_user
     @user = User.find(params[:id])
   end
