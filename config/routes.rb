d9e65c656c1a1ea5217027a32a04fb74a9e88e45
fixel92 <goodzoon92@gmail.com>
2020-02-08 16:44:38 +0300

    added private policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'events#index'
+
+  get '/pages/:page' => 'pages#show'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
